7c2d83cd029861803e8c8d69c300df0962075922
i.shesholko <i.shesholko@btslogistics.com>
2021-09-15 10:51:58 +0300

    q2

--- src/main/java/com/example/partialmerge/Test3.java
+++ src/main/java/com/example/partialmerge/Test3.java
@@ -1,4 +1,8 @@
 package com.example.partialmerge;
 
 public class Test3 {
+
+    private String filed1;
+
+    private String filed2;
 }
--- src/main/java/com/example/partialmerge/Test4.java
+++ src/main/java/com/example/partialmerge/Test4.java
@@ -1,4 +1,11 @@
 package com.example.partialmerge;
 
+import java.util.Date;
+
 public class Test4 {
+
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+
 }
