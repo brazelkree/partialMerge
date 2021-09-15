943ff5b5620688702935118eaa85e08076d5cca8
i.shesholko <i.shesholko@btslogistics.com>
2021-09-15 10:52:37 +0300

    q2

--- src/main/java/com/example/partialmerge/Test3.java
+++ src/main/java/com/example/partialmerge/Test3.java
@@ -5,4 +5,11 @@ public class Test3 {
     private String filed1;
 
     private String filed2;
+
+
+    public Test3(String filed1, String filed2) {
+        this.filed1 = filed1;
+        this.filed2 = filed2;
+    }
+
 }
